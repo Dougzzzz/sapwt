76bbd27dc1e2b371eadf60912702a9df3f9a7875
Dougzzzz <douglasbarcelos02@gmail.com>
2023-12-04 12:16:23 -0300

    até o passo 11

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/XMLView"
+    "sap/ui/core/ComponentContainer"
 
-], function(XMLView){
+], function(ComponentContainer){
     "use strict";
 
-    XMLView.create({
-        viewName:"ui5.walkthrough.view.App"    
-    }).then((oView) => oView.placeAt("content"));
+    new ComponentContainer({
+        name: "ui5.walkthrough",
+        settings : {
+            id: "walkthrough"
+        },
+        async: true
+    }).placeAt("content")
 });
